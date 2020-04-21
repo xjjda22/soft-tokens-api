9e6050e6409630ac530b2de1edff8b5d6eb10901
harryranakl <ranahari@gmail.com>
2020-04-22 00:55:44 +0800

    v0.0.2

---
+++ helpers/util.js
@@ -0,0 +1,35 @@
+// util.js
+
+const isEmpty = val => {
+	const typeOfVal = typeof val;
+	let flag = false;
+	let str = '';
+	switch (typeOfVal) {
+		case 'object': {
+			flag = val.length === 0 || !Object.keys(val).length;
+			break;
+		}
+		case 'string': {
+			str = val.trim();
+			flag = str === '' || str === undefined;
+			break;
+		}
+		case 'number': {
+			flag = val === '';
+			break;
+		}
+		default: {
+			flag = val === '' || val === undefined;
+		}
+	}
+	return flag;
+};
+
+const randomIntInc = (low, high) => {
+  return Math.floor(Math.random() * (high - low + 1) + low)
+}
+
+module.exports = {
+	isEmpty,
+	randomIntInc
+};
